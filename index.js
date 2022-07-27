239a98f2b9db0deeef0af8193f3ad5737aacb493
vkaukeano <von.kaukeano@temple.edu>
2022-07-27 10:26:48 -0400

    Event - Added Event listener. - Runs when user has interacted with commands.

--- index.js
+++ index.js
@@ -34,4 +34,17 @@ client.on('interactionCreate', async interaction => {
 	}
 });
 
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
 client.login(process.env.DISCORD_TOKEN);
